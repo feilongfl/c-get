307e77cab54f491233ee9350b2ea4d62eaf7dc23
feilong <feilongphone@gmail.com>
2019-01-09 11:48:40 +0000

    update build script

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
 echo upx info
 upx -h
+mkdir build
 echo build linux ...
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o c-get-linux-amd64 -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./build/c-get-linux-amd64 -ldflags "-s -w -X command._version_='"`git describe --abbrev=0 --tags`"' -X command._commit_='"`git log --pretty=format:"%h" -1`"'" .
 echo upx ...
-upx --best c-get-linux-amd64
+upx --best ./build/c-get-linux-amd64
 echo build linux arm ...
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o c-get-linux-arm -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o ./build/c-get-linux-arm -ldflags "-s -w -X command._version_='"`git describe --abbrev=0 --tags`"' -X command._commit_='"`git log --pretty=format:"%h" -1`"'" .
 echo upx ...
-upx --best c-get-linux-arm
+upx --best ./build/c-get-linux-arm
 echo build mac ...
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o c-get-mac -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./build/c-get-mac -ldflags "-s -w -X command._version_='"`git describe --abbrev=0 --tags`"' -X command._commit_='"`git log --pretty=format:"%h" -1`"'" .
 echo upx ...
-upx --best c-get-mac
+upx --best ./build/c-get-mac
 echo build windows ...
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o c-get-win.exe -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./build/c-get-win.exe -ldflags "-s -w -X command._version_='"`git describe --abbrev=0 --tags`"' -X command._commit_='"`git log --pretty=format:"%h" -1`"'" .
 echo upx ...
-upx --best c-get-win.exe
+upx --best ./build/c-get-win.exe
