5f97c8745b4ef8f537bfdc4b8d4e04fd161e3fc5
fei long <feilongphone@gmail.com>
2019-01-09 08:26:52 +0800

    Update build.sh
    drop debug info

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 echo build linux ...
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o c-get-linux-amd64 -ldflags "-X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o c-get-linux-amd64 -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
 echo build linux arm ...
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o c-get-linux-arm -ldflags "-X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o c-get-linux-arm -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
 echo build mac ...
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o c-get-mac -ldflags "-X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o c-get-mac -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
 echo build windows ...
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o c-get-win.exe -ldflags "-X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o c-get-win.exe -ldflags "-s -w -X main._version_='"`git describe --abbrev=0 --tags`"' -X main._commit_='"`git log --pretty=format:"%h" -1`"'" .
